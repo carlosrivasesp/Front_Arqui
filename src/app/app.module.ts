cb3e553d01eb26a90024dbca6e3b3718f53e43be
carlosrivasesp <202110052@urp.edu.pe>
2024-06-05 13:01:45 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { ProductosComponent } from './productos/productos.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { VistaClienteComponent } from './vista-cliente/vista-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaPrincipalComponent,
+    ProductosComponent,
+    CategoriasComponent,
+    VistaClienteComponent
   ],
   imports: [
     BrowserModule,
